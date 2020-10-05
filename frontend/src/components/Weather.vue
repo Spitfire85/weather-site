fbd6320a757d6196cb35b3160d2042ddf01d3f66
MattClark48 <mattclark48@hotmail.com>
2020-10-05 14:29:46 +0100

    London temp shown

---
+++ frontend/src/components/Weather.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>London is {{ londonTemp }}°C</h1>    
+</template>
+
+<script>
+import axios from 'axios';
+import cityTemps from '../data/climate.json';
+
+export default {
+  name: 'Weather',
+  data () {
+    return {
+      londonTemp: null
+    }
+  },
+  mounted () {
+    this.getLondonTemp();
+    console.log(cityTemps);
+  },
+  methods: {
+    getLondonTemp() {
+      axios
+      .get('http://api.weatherstack.com/current?access_key=0f01a0c3824f82eeb73640ed6734f813&query=London')
+      .then(response => (this.londonTemp = response.data.current.temperature))
+    }
+  }
+}
+</script>
